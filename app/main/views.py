6391629e6e359b3de1b1c42061b88e8309cb93a1
claudiahW <murungiclaud@gmail.com>
2021-11-15 23:20:12 +0300

    initial commit

---
+++ app/main/views.py
@@ -0,0 +1,16 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+# from ..requests import get_movies,get_movie,search_movie
+# from .forms import ReviewForm
+# from ..models import Review
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    message = 'Hello World'
+    return render_template('index.html',message = message)
