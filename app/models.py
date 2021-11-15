d08eb7d7334af5c398069d262edd70a813a96baf
claudiahW <murungiclaud@gmail.com>
2021-11-16 01:40:21 +0300

    create manage shell

--- app/models.py
+++ app/models.py
@@ -4,7 +4,15 @@ class Quotes:
     self.author = author
     self.quote = quote
 
+from . import db
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
 
 
 
