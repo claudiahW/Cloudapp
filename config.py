57777d85062c26db134eb12d67cb41eedfbd1d3f
claudiahW <murungiclaud@gmail.com>
2021-11-17 15:03:52 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -1,20 +1,19 @@
 import os
-
 class Config:
-
-   
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://claudbae:12345@localhost/claudapp2'
-
-
-class ProdConfig(Config):
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+class TestConfig(Config):
     pass
-
-
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    pass
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://claudbae:12345@localhost/claudapp2'
     DEBUG = True
-
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
 }
